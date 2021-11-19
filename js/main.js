2c29b4e358d2f548a2dc42ce6223e0080b94b4a3
Alex <oleksandrilnytskyi@gmail.com>
2021-11-19 12:08:25 +0200

    some changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-let burgerOpen = document.querySelector('.open__button')
-let burgerClose = document.querySelector('.close__button')
-let menu = document.querySelector('.menu__burger')
+const burgerOpen = document.querySelector('.open__button')
+const burgerClose = document.querySelector('.close__button')
+const menu = document.querySelector('.menu__burger')
+const body = document.querySelector('body')
 
 
 let toggle = false;
@@ -17,12 +18,18 @@ setInterval(() => {
     if (toggle == true) {
         menu.style = `
             transform: translate(0);
-            transition-duration: 1500ms;
+            transition-duration: 1000ms;
+        `;
+        body.style = `
+            overflow: hidden;
         `
     }else{
         menu.style = `
             transform: translate(100%);
-            transition-duration: 1500ms;
+            transition-duration: 1000ms;
+        `;
+        body.style = `
+            overflow: visible;
         `
     }
 }, 100)
